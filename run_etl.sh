76294dcbc803b24868bafa941bc668d1d104bf7a
MAkbarD <mad4869@gmail.com>
2024-03-05 00:45:36 +0700

    Finishing up

--- run_etl.sh
+++ run_etl.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Virtual Environment Path
-VENV_PATH="/home/etl-venv/bin/activate"
+VENV_PATH="/home/mad4869/Documents/pacmann/data-engineering/etl-pipeline/etl-venv/bin/activate"
 
 # Activate Virtual Environment
 source "$VENV_PATH"
 
 # Set Python Script
-PYTHON_SCRIPT="/home/etl_pipeline.py"
+PYTHON_SCRIPT="/home/mad4869/Documents/pacmann/data-engineering/etl-pipeline/etl_pipeline.py"
 
 # Get Current Date
-current_date=$(date '+%d-%m-%Y')
+current_datetime=$(date '+%d-%m-%Y_%H-%M')
 
 # Append Current Date in the Log File
-log_file="/home/log/etl_$current_date.log"
+log_file="/home/mad4869/Documents/pacmann/data-engineering/etl-pipeline/logs/etl_$current_datetime.log"
 
 # Run Python Script and Insert Log
 python "$PYTHON_SCRIPT" >> "$log_file" 2>&1
